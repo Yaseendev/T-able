07ce6a17c4243d8d69c0276335f22152127dd6b8
Yaseen Hasanen <yaseen.hasanen@gmail.com>
2020-10-30 13:45:10 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/Home_Screen.dart';
+import 'package:analog_clock/analog_clock.dart';
 
 void main() {
   runApp(SmartProductivityApp());
@@ -10,7 +11,24 @@ class SmartProductivityApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'T-able',
-      initialRoute: HomeScreen.id,
+      home:  AnalogClock(
+	decoration: BoxDecoration(
+	    border: Border.all(width: 2.0, color: Colors.black),
+	    color: Colors.transparent,
+	    shape: BoxShape.circle),
+	width: 150.0,
+	isLive: true,
+	hourHandColor: Colors.black,
+	minuteHandColor: Colors.black,
+	showSecondHand: false,
+	numberColor: Colors.black87,
+	showNumbers: true,
+	textScaleFactor: 1.4,
+	showTicks: false,
+	showDigitalClock: false,
+	datetime: DateTime(2019, 1, 1, 9, 12, 15),
+	),
+      //initialRoute: HomeScreen.id,
       routes: {
         HomeScreen.id : (context) =>HomeScreen(),
       },
