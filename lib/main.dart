fbc36dfbee2b3044c7191bc3e357f0f0fcbf6e2e
Yaseen Hasanen <yaseen.hasanen@gmail.com>
2020-11-05 22:30:17 +0200

    Editing the home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './screens/Home_Screen.dart';
-import 'package:analog_clock/analog_clock.dart';
+//import 'package:analog_clock/analog_clock.dart';
 
 void main() {
   runApp(SmartProductivityApp());
@@ -11,25 +11,7 @@ class SmartProductivityApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'T-able',
-      home:  Scaffold(
-              body: AnalogClock(
-	decoration: BoxDecoration(
-	    border: Border.all(width: 2.0, color: Colors.black),
-	    color: Colors.transparent,
-	    shape: BoxShape.circle),
-	width: 150.0,
-	isLive: true,
-	hourHandColor: Colors.black,
-	minuteHandColor: Colors.black,
-	showSecondHand: false,
-	numberColor: Colors.black87,
-	showNumbers: true,
-	textScaleFactor: 1.4,
-	showTicks: false,
-	showDigitalClock: false,
-	datetime: DateTime(2019, 1, 1, 9, 12, 15),
-	),
-      ),
+      home: HomeScreen(),
       //initialRoute: HomeScreen.id,
       routes: {
         HomeScreen.id : (context) =>HomeScreen(),
--- lib/screens/Home_Screen.dart
+++ lib/screens/Home_Screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:analog_clock/analog_clock.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String id = 'home_screen';
@@ -10,22 +11,57 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return AnalogClock(
-	decoration: BoxDecoration(
-	    border: Border.all(width: 2.0, color: Colors.black),
-	    color: Colors.transparent,
-	    shape: BoxShape.circle),
-	width: 150.0,
-	isLive: true,
-	hourHandColor: Colors.black,
-	minuteHandColor: Colors.black,
-	showSecondHand: false,
-	numberColor: Colors.black87,
-	showNumbers: true,
-	textScaleFactor: 1.4,
-	showTicks: false,
-	showDigitalClock: false,
-	datetime: DateTime(2019, 1, 1, 9, 12, 15),
-	);
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background.png'),
+            fit: BoxFit.cover,
+          ),
+          ),
+         child: SafeArea(
+                child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: AnalogClock(
+                    decoration: BoxDecoration(
+                        //border: Border.all(width: 2.0, color: Colors.black),
+                        color: Colors.transparent,
+                        shape: BoxShape.circle,
+                        ),
+                    isLive: true,
+                    hourHandColor: Colors.yellow,
+                    minuteHandColor: Colors.amber,
+                    showSecondHand: true,
+                    numberColor: Colors.blue,
+                    showNumbers: true,
+                    textScaleFactor: 1.4,
+                    showTicks: true,
+                    showDigitalClock: true,
+                    digitalClockColor: Colors.blueAccent,
+                    datetime: DateTime(2019, 1, 1, 9, 12, 15),
+                  ),
+                ),
+              ),
+              Expanded(flex: 1, child: Container()),
+            ],
+          ),
+        ),
+      ),
+       bottomNavigationBar: CurvedNavigationBar(
+    backgroundColor: Colors.blueAccent,
+    items: <Widget>[
+      Icon(Icons.list, size: 30),
+      Icon(Icons.add, size: 30),
+      Icon(Icons.compare_arrows, size: 30),
+    ],
+    onTap: (index) {
+      //Handle button tap
+    },
+  ),
+    );
   }
 }
